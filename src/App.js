224cdbc915450f46bd2576bb47637a71acc0f71d
Rajendra Bhausaheb Pacharne <69509779+Rajendra-pacharne@users.noreply.github.com>
2023-08-04 17:35:11 +0530

    About page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 
 import './App.css';
+import About from './Components/About';
 import NavBar from './Components/NavBar'
-import TextForm from './Components/TextForm'
+// import TextForm from './Components/TextForm'
 
 function App() {
   return (
     <>
-   
-  <NavBar title="TextUtils" about="About Us"/>
-  
-  <div className='container' >
-  <TextForm heading="Enter the text To Analyze"/>
-  </div>
-  
+      
+      <NavBar title="TextUtils" about="About Us"/>
+      
+      <div className='container' >
+      {/* <TextForm heading="Enter the text To Analyze"/> */}
+      <About/>
+      </div>
+     
   </>
   );
 }
