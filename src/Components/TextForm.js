e2771deda64a39b8eef179475d15fbaabb231d0c
Rajendra Bhausaheb Pacharne <69509779+Rajendra-pacharne@users.noreply.github.com>
2023-08-04 16:49:19 +0530

    Copy To Clipboard feature added

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,20 +1,39 @@
 import React , {useState} from 'react'
-
+import ClipboardJS from 'clipboard';
 export default function TextForm(props) {
     const haddleupclick=(event)=> {
         console.log("Upper clicked Was Pressed")
         let newText=text.toUpperCase();
         setText(newText)
-    }
+    };
     const haddledclick=(event)=> {
       console.log("Lowercase clicked Was Pressed")
       let newText=text.toLowerCase();
       setText(newText)
-  }
+  };
+  const haddleClearclick=(event)=> {
+    console.log("haddleClearclick clicked Was Pressed")
+    let newText='';
+    setText(newText)
+};
+const haddlecopyclick = (event) => {
+  // Create a new ClipboardJS instance
+  const clipboard = new ClipboardJS('.btn-copy', {
+    text: () => text,
+  });
+
+  // Trigger the copy action
+  clipboard.onClick(event);
+
+  // Destroy the ClipboardJS instance to clean up
+  clipboard.destroy();
+
+  console.log('Text copied to clipboard');
+};
     const haddleOnChange=(event)=> {
         console.log("haddleOnChange ");
         setText(event.target.value)
-    }
+    };
     const [text, setText] = useState( 'Enter text here');
   return (
     <>
@@ -26,6 +45,9 @@ export default function TextForm(props) {
           </div>
           <button className="btn btn-primary mx-2"onClick={haddleupclick} > Convert to Uppercase </button>
           <button className="btn btn-primary mx-2"onClick={haddledclick} > Convert to LowerCase </button>
+          <button className="btn btn-primary mx-2"onClick={haddleClearclick} > Clear Text </button>
+          <button className="btn btn-primary mx-2"onClick={haddlecopyclick} > Copy To ClipedBoard </button>
+ 
   </div>
   <div className='container my-3'>
     <h2>Your Text Summary</h2>
@@ -35,6 +57,5 @@ export default function TextForm(props) {
     <p>{text}</p>
     </div>
   </>
-    
   )
 }
