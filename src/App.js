3067cf4b9dfec72cce4a328a93040b5e8e913131
Rajendra Bhausaheb Pacharne <69509779+Rajendra-pacharne@users.noreply.github.com>
2023-08-05 17:16:52 +0530

    fevicon and popups

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import React , {useState} from 'react'
 
 
 function App() {
-  const [mode, setMode] = useState('dark'); 
+  const [mode, setMode] = useState('light'); 
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type)=>{
@@ -26,12 +26,22 @@ function App() {
       {
         setMode('light');
         showAlert("light mode has been enabled", "success");
+        document.title="TextUtils-Light Mode"
         document.body.style.backgroundColor='white';
+        // setInterval( ()=>
+        // {
+        //   // alert("TextUti1s is Amazing Mode");
+        //   window.alert("TextUti1s is Amazing Website");
+        // },1000
+        // );
+        // Hacked 
+
       }
       else
       {
         setMode('dark');
         showAlert("Dark mode has been enabled", "success");
+        document.title="TextUtils-Dark Mode"
         document.body.style.backgroundColor='grey';
       }
   }
